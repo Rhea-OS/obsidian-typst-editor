c5cb7e0084adea18558fd55d1aac74a288cb802f
itschnj2 <jacob.schneider@med.uni-tuebingen.de>
2025-05-06 14:40:08 +0200

    Standard view erstellt

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,23 @@
 import * as React from 'react';
 import * as dom from 'react-dom/client';
 import * as obs from 'obsidian';
+import {WorkspaceLeaf} from "obsidian";
+
+export const TYPST_VIEW = 'typst-view';
 
 export default class TypstEditor extends obs.Plugin {
+	async onload() {
+		this.registerView(TYPST_VIEW, leaf => new TypstView(leaf, this));
+		this.registerExtensions(["typst"], TYPST_VIEW);
+	}
+}
 
+export class TypstView extends obs.EditableFileView {
+	constructor(readonly leaf: WorkspaceLeaf, private plugin: TypstEditor) {
+		super(leaf);
+	}
+
+    getViewType(): string {
+        return TYPST_VIEW;
+    }
 }
